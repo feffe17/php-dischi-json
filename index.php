fe6f58f6b83b670a51b5aee8a726becddb251ac0
feffe1702 <fpm.asd@gmail.com>
2025-02-21 12:04:54 +0100

    [WIP] Add initial CD data and display structure in index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+$cds_list = file_get_contents("./cds.json");
+$cds_list = json_decode($cds_list, true);
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous" />
+</head>
+
+<body>
+    <header>
+        <h1 class="text-center">titolo</h1>
+    </header>
+
+    <body>
+        <?php
+        echo "<ul class='list-group d-flex'>";
+        foreach ($cds_list as $cd) {
+            echo "<li class='col-4'>";
+            foreach ($cd as $key => $value) {
+                echo "<strong>$value</strong> ";
+            }
+            echo "</li>";
+        }
+        echo "</ul>";
+        ?>
+    </body>
+    <script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
